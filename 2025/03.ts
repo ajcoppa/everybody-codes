ecc2120b11cf22852af1fba4614c39805908bb63
AJ Coppa <aj.coppa@gmail.com>
2025-11-27 21:47:04 -0500

    3**

--- 2025/03.ts
+++ 2025/03.ts
@@ -2,6 +2,9 @@ async function main() {
     const partOneText = await Bun.file("03-input.txt").text();
     const partOneInput = parseInput(partOneText);
     console.log(partOne(partOneInput));
+    const partTwoText = await Bun.file("03-input-2.txt").text();
+    const partTwoInput = parseInput(partTwoText);
+    console.log(partTwo(partTwoInput));
 }
 
 function partOne(xs: number[]): number {
@@ -11,6 +14,17 @@ function partOne(xs: number[]): number {
         sum += n;
     }
     return sum;
+}
+
+function partTwo(xs: number[]): number {
+    const unique = new Set(xs.sort((a, b) => a - b));
+    let sum = 0, crates = 0;
+    for (const n of unique) {
+        sum += n;
+        crates++;
+        if (crates === 20) return sum;
+    }
+    throw new Error("Didn't have enough crates");
 }
 
 function parseInput(text: string): number[] {
